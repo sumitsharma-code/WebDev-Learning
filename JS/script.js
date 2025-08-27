c9ce0416c6858d5d4a5f542f2afd8bbedff0d1ac
sumitsharma-code <97402908+sumitsharma-code@users.noreply.github.com>
2025-08-27 14:03:08 +0530

    functions done

--- JS/script.js
+++ JS/script.js
@@ -232,7 +232,7 @@
 
 // typeof
 //qrks
-// null - 'object' 
+// null - 'object'
 // array - 'object'
 // NaN - 'number'
 
@@ -240,4 +240,182 @@
 // usse janam liya hua
 // a instanceof Array;
 
+// Switch
+// switch (5) {
+//   case 2:
+//     console.log("hehe");
+//     break;
+//   case 4:
+//     console.log("hehe");
+//     break;
+//   default:
+//     console.log("A");
+// }
 
+// function doit(val) {
+//   if (val < 100) {
+//     if (val < 50) {
+//       if (val < 25) {
+//         if (val < 5) {
+//           console.log("d");
+//           return;
+//         }
+//         console.log("c");
+//         return;
+//       }
+//       console.log("b");
+//       return;
+//     }
+//     console.log("a");
+//     return;
+//   }
+// }
+
+// doit(49);
+
+// Rock-paper-scissors logic
+
+// function rps(user, computer) {
+//   if (user === computer) return "draw";
+//   // RS
+//   if (user === "rock" && computer === "scissors") return "user";
+
+//   // RP
+//   if (user === "paper" && computer === "rock") return "user";
+
+//   // SP
+//   if (user === "scissors" && computer === "paper") return "user";
+
+//   return "computer"
+// }
+// console.log(rps("scissors", "paper"));
+
+// Loops
+
+// For
+
+// for(let a = 0; a < 12; a++)
+// {
+//   console.log(a);
+// }
+
+// While
+// let a = 0;
+// while (a < 3) {
+//   console.log(a);
+//   a++;
+// }
+
+// Do while
+// let i = 12;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 2);
+
+// q1
+// for(let i = 1; i < 11; i++)
+// {
+//   console.log(i);
+// }
+
+// q2
+// let a = 10;
+// while (a > 0) {
+//   console.log(a);
+//   a--;
+// }
+
+// let a = prompt("give a number");
+// if(a == 2)
+// {
+//   console.log("wah");
+// }
+
+// functions
+
+// `...` - Rest operator
+// function huaa(...val)
+// {
+//   console.log(typeof(val));
+// }
+
+// huaa(1,2,4,5,4)
+
+// First Class functions
+// function discountCal(discount) {
+//   return function (price) {
+//     return price - price * (discount/100);
+//   };
+// }
+
+// let mydisc = discountCal(10);
+// console.log(mydisc(110));
+
+// Arrays
+// let mars = [0, 1, 2, 3, 4, 5];
+// console.log(mars[2]);
+// let arr = new Array();
+// mars.push(32); // Add at last
+// console.log(mars);
+// mars.pop(); // remove from last
+// console.log(mars);
+// mars.shift(); // remove from front
+// console.log(mars);
+// mars.unshift(5); // Add at front
+// console.log(mars);
+// mars.splice(2,2);
+// // mars.splice(start_index, how many values)
+// console.log(mars);
+
+// mars = [0, 1, 2, 3, 4, 5];
+// console.log(mars);
+// let newArr = mars.slice(0,4); // returns new array with sliced values
+// mars.slice(start_index, end_index{not included});
+// console.log(newArr);
+
+// mars.reverse();
+
+// let sr = mars.sort(function(a, b)  {
+//   return b - a;
+// });
+
+// console.log(mars);
+
+// For each
+// let mars = [0, 1, 2, 3, 4, 5];
+
+// mars.forEach(function(val){
+//   console.log(val);
+// });
+
+// let mars = [0, 1, 2, 3, 4, 5];
+// // Maps
+// // use when u want to create a new array
+
+// // Dikhte hi sath black array bnalo
+// let newarr = mars.map(function(val){
+//   if (val > 1) return 12;
+// });
+
+// jaha bhi ek array se new array bnega aur vo array kuch values ko rkhega tab map lgega
+
+// let mars = [0, 1, 2, 3, 4, 5];
+// Filter
+// let newArr = mars.filter(function(val){
+//   if (val > 2) return true;
+// });
+
+// let mars = [0, 1, 2, 3, 4, 5];
+// Reduce
+// when we want a single number at last
+// let newArr = mars.reduce(function (accumulator, val) {
+//   return accumulator - val;
+// }, 0);
+// this 0 is the value of accumulator at the start
+
+// let obj = {
+//   name: "sumit",
+//   age: 21,
+//   khana: "Kheer"
+// };
